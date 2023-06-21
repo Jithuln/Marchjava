b68fdc6de6097632bf9f978c532d0fb9da033806
JITHUL N <jithuljithu2@gmail.com>
2023-06-21 23:50:52 +0530

    First commit

---
+++ src/gitdemo/Gittest.java
@@ -0,0 +1,27 @@
+package gitdemo;
+
+public class Gittest {
+
+	public static void main(String[] args) {
+		
+
+			int a,b,c,d;
+			float f=7.89f,g;
+			a=20;
+			b=78;
+			c=a+b;
+			System.out.println("result ="+c);
+			//adding num with decimal
+			d=56;
+			g=d+f;
+			System.out.println("result ="+g);
+			
+			
+		
+			
+
+		}
+
+	
+
+}
